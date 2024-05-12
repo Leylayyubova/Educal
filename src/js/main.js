913d81fb19bc896490299e8a2572549d646eadac
Leyla Ayyubova <102313083+Leylayyubova@users.noreply.github.com>
2024-05-13 00:51:12 +0400

    educal/js

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    let x = document.querySelector(".menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+
+    // let y = document.querySelector(".form");
+    // if (y.style.display === "block") {
+    //   y.style.display = "none";
+    // } else {
+    //   y.style.display = "block";
+    // }
+  }
